561d388476d9869b9dd72f568bf5152bd1e1b156
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-05-04 16:24:43 -0300

    Removendo caracteres indesejados no telefone, gerando TOKEN randômico
    replace - substitui uma informação, por outra desejada.
    Gerar TOKEN - NÃO RECOMENDADO - apenas para estudo!
    Pegar às Strings

--- Projetos_Android_JAVA/Whatsapp/app/src/main/java/com/br/whatsapp/activity/LoginActivity.java
+++ Projetos_Android_JAVA/Whatsapp/app/src/main/java/com/br/whatsapp/activity/LoginActivity.java
@@ -2,15 +2,21 @@ package com.br.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.br.whatsapp.R;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
+import java.util.Random;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText editTextNome, editTextCodPais, editTextCodArea, editTextTelefone;
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,31 @@ public class LoginActivity extends AppCompatActivity {
         findViews();
         simplesMaskFormatter();
 
+        buttonCadastro();
+
+    }
+
+    //Botão cadastro
+    public void buttonCadastro() {
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Pegar às Strings
+                String nomeUsuario = editTextNome.getText().toString();
+                String telefoneCompleto = editTextCodPais.getText().toString() + editTextCodArea.getText().toString() + editTextTelefone.getText().toString();
+
+                //replace - substitui uma informação, por outra desejada.
+                String telefoneSemFormatacao = telefoneCompleto.replace("+", "");
+                telefoneSemFormatacao = telefoneSemFormatacao.replace("-", "");
+
+                //Gerar TOKEN - NÃO RECOMENDADO - apenas para estudo!
+                Random randomico = new Random();
+                int numeroRandimico = randomico.nextInt(9999 - 1000) + 1000;
+                String token = String.valueOf(numeroRandimico);  //Converter String para o que deseja
+            }
+        });
     }
 
     //Buscar os Views pelo ID
@@ -29,13 +60,14 @@ public class LoginActivity extends AppCompatActivity {
         editTextTelefone = findViewById(R.id.editTextTelefoneID);
         editTextCodPais = findViewById(R.id.editTextCodPais);
         editTextCodArea = findViewById(R.id.editTextCodArea);
+        btnCadastrar = findViewById(R.id.buttonCadastrar);
     }
 
     //Simples máscara de formatação
 
     public void simplesMaskFormatter() {
 
-        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("N NNNN-NNNN");
+        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
         SimpleMaskFormatter simpleMaskPais = new SimpleMaskFormatter("+NN");
         SimpleMaskFormatter simpleMaskArea = new SimpleMaskFormatter("NN");
 
