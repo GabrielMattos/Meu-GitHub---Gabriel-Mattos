5193509c178039e91db6a36651205154ea19d2d4
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-05-11 21:41:53 -0300

    Deslogar usuario / configurações toolbar

--- Projetos_Android_JAVA/Whatsapp/app/src/main/java/com/br/whatsapp/activity/MainActivity.java
+++ Projetos_Android_JAVA/Whatsapp/app/src/main/java/com/br/whatsapp/activity/MainActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.br.whatsapp.R;
 import com.br.whatsapp.config.ConfiguracaoFirebase;
@@ -18,13 +20,14 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar myToolBar;
-
-
+    private FirebaseAuth usuarioAtenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usuarioAtenticacao = ConfiguracaoFirebase.getFirebaseAntenticacao();
 
         myToolBar = findViewById(R.id.toolbarPrincipal);
         myToolBar.setTitle("WhatsApp");
@@ -37,9 +40,60 @@ public class MainActivity extends AppCompatActivity {
 
         MenuInflater myInflater = getMenuInflater();
         myInflater.inflate(R.menu.menu_main, menu);
-
-
-
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.action_configuracoes:
+                return true;
+            default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void deslogarUsuario() {
+
+        usuarioAtenticacao.signOut();
+        Intent myIntent = new Intent(MainActivity.this, LoginActivity.class);
+        Toast.makeText(this, "Sucesso ao deslogar usuário", Toast.LENGTH_SHORT).show();
+        startActivity(myIntent);
+        finish();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
